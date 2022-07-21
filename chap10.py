76ff078726e1085914ea7946391b3974cfe1fd65
gunhee27 <gunhee27@naver.com>
2022-07-22 01:00:38 +0900

    220722

---
+++ chap10.py
@@ -0,0 +1,105 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b :
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+for i in range(n + 1):
+    parent[i] = i
+
+for i in range(m):
+    oper, a, b = map(int, input().split())
+    if oper == 0:
+        union_parent(parent, a, b)
+    elif oper == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('Yes')
+        else:
+            print('no')
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+edges = []
+result = 0
+
+for i in range(1, v+ 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+last = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - last)
+
+from collections import deque
+import copy
+
+v = int(input())
+indegree = [0] * (v + 1)
+graph = [[] for i in range(v + 1)]
+time = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    data = list(map(int, input().split()))
+    time[0] = data[0]
+    for x in data[1: -1]:
+        indegree += 1
+        graph[x].append(i)
+
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
+
+    for i in range(1, v + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i], result[now] + time[i])
+            indegree[i] -= 1
+
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in range(1, v + 1):
+        print(result[i])
+
+topology_sort()
