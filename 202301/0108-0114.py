2b6c1676c1a518ef173b72241f777b5c9b4b8a7b
gunhe <gunhee27@naver.com>
2023-01-10 22:29:45 +0900

    230110

--- 202301/0108-0114.py
+++ 202301/0108-0114.py
@@ -18,3 +18,39 @@ while left <= right:
     else:
         left = mid + 1
 print(right)
+
+###############################
+# 0110
+###############################
+# 1541
+arr = input().split('-')
+result = []
+for i in arr:
+    if i.find('+') >= 0:
+        num = 0
+        temp = i.split('+')
+        for j in temp:
+            num += int(j)
+        result.append(num)
+    else:
+        result.append(int(i))
+print(result[0] - sum(result[1:]))
+
+# 1654
+k, n = map(int, input().split())
+arr = []
+for i in range(k):
+    arr.append(int(input()))
+
+left = 1
+right = max(arr)
+while left <= right:
+    mid = (left + right) // 2
+    s = 0
+    for i in arr:
+        s += (i // mid)
+    if s >= n:
+        left = mid + 1
+    else:
+        right = mid - 1
+print(right)
