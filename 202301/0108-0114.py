e73d8fe66268766137e434e1cafe0e9f86896a65
gunhe <gunhee27@naver.com>
2023-01-11 21:58:20 +0900

    230111

--- 202301/0108-0114.py
+++ 202301/0108-0114.py
@@ -54,3 +54,74 @@ while left <= right:
     else:
         right = mid - 1
 print(right)
+
+###############################
+# 0111
+###############################
+# 14889
+n = int(input())
+arr = []
+for t in range(n):
+    arr.append(list(map(int, input().split())))
+visited = [False] * (n + 1)
+min_diff = 1e9
+
+
+def dfs(depth, index):
+    global min_diff
+    if depth == (n // 2):
+        start = 0
+        link = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if visited[i] and visited[j]:
+                    start += arr[i][j]
+                    start += arr[j][i]
+                elif not visited[i] and not visited[j]:
+                    link += arr[i][j]
+                    link += arr[j][i]
+        min_diff = min(min_diff, abs(start - link))
+
+    for k in range(index, n):
+        if not visited[k]:
+            visited[k] = True
+            dfs(depth + 1, k + 1)
+            visited[k] = False
+
+
+dfs(0, 0)
+print(min_diff)
+
+# 1972
+import heapq
+import sys
+
+n = int(input())
+q = []
+for _ in range(n):
+    a = int(sys.stdin.readline())
+    if a > 0:
+        heapq.heappush(q, a)
+    elif a == 0:
+        if q:
+            min_num = heapq.heappop(q)
+            print(min_num)
+        else:
+            print(0)
+
+# 11279
+import heapq
+import sys
+
+n = int(input())
+q = []
+for _ in range(n):
+    a = int(sys.stdin.readline())
+    if a > 0:
+        heapq.heappush(q, a * (-1))
+    elif a == 0:
+        if q:
+            max_num = heapq.heappop(q)
+            print(max_num * (-1))
+        else:
+            print(0)
