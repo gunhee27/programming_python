f24118bbfec50aeaf9f57acd2cd7df0b96a88f22
gunhe <gunhee27@naver.com>
2023-01-12 22:21:10 +0900

    230112

--- 202301/0108-0114.py
+++ 202301/0108-0114.py
@@ -125,3 +125,76 @@ for _ in range(n):
             print(max_num * (-1))
         else:
             print(0)
+
+
+###############################
+# 0112
+###############################
+# 4963
+import sys
+sys.setrecursionlimit(10000)
+
+dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+dy = [0, 0, -1, 1, 1, -1, 1, -1]
+
+def dfs(x, y):
+    visited[x][y] = 1
+    if arr[x][y] == 1:
+        for k in range(8):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < h and 0 <= ny < w:
+                if arr[nx][ny] == 1 and not visited[nx][ny]:
+                    dfs(nx, ny)
+
+while 1:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    arr = []
+    visited = [[0] * w for _ in range(h)]
+    for i in range(h):
+        arr.append(list(map(int, input().split())))
+    answer = 0
+    for a in range(h):
+        for b in range(w):
+            if arr[a][b] == 1 and not visited[a][b]:
+                dfs(a, b)
+                answer += 1
+            else: continue
+
+    print(answer)
+#######################################################
+from collections import deque
+
+dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+dy = [0, 0, -1, 1, 1, -1, 1, -1]
+
+def bfs(x, y):
+    visited[x][y] = 1
+    q = deque([(x, y)])
+    while q:
+        x, y = q.popleft()
+        for i in range(8):
+            na = x + dx[i]
+            nb = y + dy[i]
+            if 0 <= na < h and 0 <= nb < w:
+                if arr[na][nb] == 1 and not visited[na][nb]:
+                    visited[na][nb] = 1
+                    q.append((na, nb))
+
+while 1:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    arr = []
+    visited = [[0] * w for _ in range(h)]
+    for i in range(h):
+        arr.append(list(map(int, input().split())))
+    answer = 0
+    for a in range(h):
+        for b in range(w):
+            if arr[a][b] == 1 and not visited[a][b]:
+                bfs(a, b)
+                answer += 1
+    print(answer)
