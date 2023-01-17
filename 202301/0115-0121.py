fa0dc399bbbc2b24099e30a529d997973459cca3
gunhe <gunhee27@naver.com>
2023-01-17 21:51:04 +0900

    230117

--- 202301/0115-0121.py
+++ 202301/0115-0121.py
@@ -23,3 +23,35 @@ for i in range(n):
 if flag == 0:
     for i in answer:
         print(i)
+
+###############################
+# 0117
+###############################
+# 10799
+n = input()
+stack = []
+count = 0
+for i in range(len(n)):
+    if n[i] == '(':
+        stack.append('(');
+    else:
+        stack.pop()
+        if n[i - 1] == '(':
+            count += len(stack)
+        else:
+            count += 1
+print(count)
+
+# 6603
+from itertools import combinations
+while 1:
+    n = list(map(int, input().split()))
+    if n[0] == 0: break
+    k = n[0]
+    arr = n[1:]
+    result = list(combinations(arr, 6))
+    for i in result:
+        for j in i:
+            print(j, end=' ')
+        print()
+    print()
