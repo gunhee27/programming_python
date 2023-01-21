d546e7fca2034c1c040bca4ad8dec002c259f130
gunhe <gunhee27@naver.com>
2023-01-21 22:49:45 +0900

    230121

--- 202301/0115-0121.py
+++ 202301/0115-0121.py
@@ -86,3 +86,53 @@ def bfs(x, y):
     return arr[n - 1][m - 1]
 
 print(bfs(0, 0))
+
+###############################
+# 0121
+###############################
+# 2667
+import sys
+sys.setrecursionlimit(10000)
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().rstrip())))
+
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+
+def dfs(x, y):
+    if 0 <= x < n and 0 <= y < n and arr[x][y] == 1:
+        global count
+        count += 1
+        arr[x][y] = 0
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            dfs(nx, ny)
+        return True
+    return False
+
+num = []
+count = 0
+for i in range(n):
+    for j in range(n):
+        if dfs(i, j) == True:
+            num.append(count)
+            count = 0
+print(len(num))
+num.sort()
+for i in num:
+    print(i)
+
+# 1149
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    arr[i][0] = min(arr[i - 1][1], arr[i - 1][2]) + arr[i][0]
+    arr[i][1] = min(arr[i - 1][0], arr[i - 1][2]) + arr[i][1]
+    arr[i][2] = min(arr[i - 1][1], arr[i - 1][0]) + arr[i][2]
+print(min(arr[n - 1]))
