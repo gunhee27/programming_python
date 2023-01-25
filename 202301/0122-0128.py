77b234b79e2032d3e417a1b960c7d84f3b7b1881
gunhe <gunhee27@naver.com>
2023-01-25 22:26:42 +0900

    230125

--- 202301/0122-0128.py
+++ 202301/0122-0128.py
@@ -15,3 +15,26 @@ for i in range(1, n):
         cnt += 1
         end = arr[i][1]
 print(cnt)
+
+###############################
+# 0125
+###############################
+# 1697
+from collections import deque
+n, k = map(int, input().split())
+visited = [0] * 100001
+
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        a = q.popleft()
+        if a == k:
+            print(visited[a])
+            break
+        for i in (a - 1, a + 1, 2 * a):
+            if 0 <= i <= 100000 and not visited[i]:
+                visited[i] = visited[a] + 1
+                q.append(i)
+
+bfs()
