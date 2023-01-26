0b11ac3b7151f200c5d231d577039fdcc9b41605
gunhe <gunhee27@naver.com>
2023-01-26 23:12:48 +0900

    230126

--- 202301/0122-0128.py
+++ 202301/0122-0128.py
@@ -38,3 +38,37 @@ def bfs():
                 q.append(i)
 
 bfs()
+
+###############################
+# 0126
+###############################
+# 1932
+n = int(input())
+arr = [[0] * n for _ in range(n)]
+for i in range(n):
+    aa = list(map(int, input().split()))
+    for j in range(len(aa)):
+        arr[i][j] = aa[j]
+
+for i in range(1, n):
+    for j in range(n):
+        if j == 0:
+            arr[i][0] += arr[i - 1][0]
+        elif j == n - 1:
+            arr[i][j] += arr[i - 1][j - 1]
+        else:
+            arr[i][j] += max(arr[i - 1][j], arr[i - 1][j - 1])
+print(max(arr[n - 1]))
+
+# 2156
+n = int(input())
+arr = [0] * 10000
+for i in range(n):
+    arr[i] = int(input())
+d = [0] * (10000)
+d[0] = arr[0]
+d[1] = arr[0] + arr[1]
+d[2] = max(arr[0] + arr[2], arr[1] + arr[2], d[1])
+for i in range(3, n):
+    d[i] = max(d[i - 2] + arr[i], arr[i] + arr[i - 1] + d[i - 3], d[i - 1])
+print(max(d))
