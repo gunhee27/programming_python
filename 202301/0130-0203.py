88e326ac1c7240f2065ac9ea416efa3bd860ffa8
gunhe <gunhee27@naver.com>
2023-01-29 21:40:56 +0900

    230129-1

--- 202301/0130-0203.py
+++ 202301/0130-0203.py
@@ -1,6 +1,36 @@
 ###############################
 # 0129
 ###############################
+# 14888
+n = int(input())
+arr = list(map(int, input().split()))
+
+a, b, c, d = map(int, input().split())
+maxi = -1e9
+mini = 1e9
+
+def dfs(d, total, add, minus, multi, div):
+    global maxi, mini
+    if d == n:
+        maxi = max(total, maxi)
+        mini = min(total, mini)
+        return
+
+    if add:
+        dfs(d + 1, total + arr[d], add - 1, minus, multi, div)
+    if minus:
+        dfs(d + 1, total - arr[d], add, minus - 1, multi, div)
+    if multi:
+        dfs(d + 1, total * arr[d], add, minus, multi - 1, div)
+    if div:
+        dfs(d + 1, int(int(total) / arr[d]), add, minus, multi, div - 1)
+
+
+dfs(1, arr[0], a, b, c, d)
+print(maxi)
+print(mini)
+
+#1991
 n = int(input())
 tree = {}
 for i in range(n):
