f1de7d7a5ef1f9a0e8a2579e66c068601e07e4ba
gunhe <gunhee27@naver.com>
2023-02-20 21:36:06 +0900

    230220

---
+++ 백준/silver1/12852.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = [[0, []] for _ in range(n + 1)]
+arr[1][0] = 0
+arr[1][1] = [1]
+
+for i in range(2, n + 1):
+    arr[i][0] = arr[i - 1][0] + 1
+    arr[i][1] = arr[i - 1][1] + [i]
+    if i % 3 == 0 and arr[i // 3][0] + 1 < arr[i][0]:
+        arr[i][0] = arr[i // 3][0] + 1
+        arr[i][1] = arr[i // 3][1] + [i]
+    if i % 2 == 0 and arr[i // 2][0] + 1 < arr[i][0]:
+        arr[i][0] = arr[i // 2][0] + 1
+        arr[i][1] = arr[i // 2][1] + [i]
+
+print(arr[n][0])
+print(*sorted(arr[n][1], reverse=True))
