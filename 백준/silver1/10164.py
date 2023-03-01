d5811b54aefe8b94eff8a5f51fb6d0d4f82c42ea
gunhe <gunhee27@naver.com>
2023-03-01 23:52:21 +0900

    230301

---
+++ 백준/silver1/10164.py
@@ -0,0 +1,23 @@
+n, m, k = map(int, input().split())
+
+def find(x, y):
+    dp = [[0] * (y + 1) for _ in range(x + 1)]
+    for i in range(1, x + 1):
+        for j in range(1, y + 1):
+            if i == 1 and j == 1:
+                dp[i][j] = 1
+                continue
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[x][y]
+
+if k == 0:
+    print(find(n, m))
+else:
+    sx = k // m + 1
+    sy = k % m
+    if sy == 0:
+        sy = m
+    first = find(sx, sy)
+    second = find(n - sx + 1, m - sy + 1)
+    print(first * second)
+
